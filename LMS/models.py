811f677deedac17e6c89322ea636e5a854acf611
Josephshitandi <jsphshtnd@gmail.com>
2025-01-03 12:53:00 +0300

    Added AbstractUser Class

--- LMS/models.py
+++ LMS/models.py
@@ -1,3 +1,18 @@
+# models.py
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    role = models.CharField(max_length=20, choices=[('admin', 'Admin'), ('instructor', 'Instructor'), ('student', 'Student')])
+
+
+
+
+
+
+
+
+
+
 from django.db import models
 
 # Create your models here.
