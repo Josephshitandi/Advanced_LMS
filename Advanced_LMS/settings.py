08b830369c22600fb7318676a308068049b31385
Josephshitandi <jsphshtnd@gmail.com>
2025-01-10 11:26:26 +0300

    Added corsheaders

--- Advanced_LMS/settings.py
+++ Advanced_LMS/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'channels',
     'rest_framework',
+    "corsheaders",
 ]
 
 
@@ -53,7 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 
 ROOT_URLCONF = 'Advanced_LMS.urls'
 
